517ef1611cb7c4b382a2974649ccb0c1ccbe4c13
Grayson <grayson.clary@gmail.com>
2016-01-07 21:21:09 -0500

    Various

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,22 +1,39 @@
 class AnswersController < ApplicationController
   def index
-  end
-
-  def show
+    @answers = Answer.find(params[:question_id])
   end
 
   def new
+    @answer = Answer.new
   end
 
   def create
+    @answer = Answer.new(answer_params)
+
+    if @answer.save
+      redirect to question_answers_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @answer = Answer.find(params[:id])
   end
 
   def update
+    @answer = Answer.find(params[:id])
+
+    if @answer.update(answer_params)
+      redirect_to question_answers_path
+    else
+      render 'edit'
+    end
   end
 
-  def destroy
+  private
+
+  def answer_params
+    params.require(:answer).permit(:description, :upvote, :downvote, :question_id)
   end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,22 +1,44 @@
 class QuestionsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect to authenticated_root_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @question = Question.find(params[:id])
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
+    @question = Question.find(params[:id])
+
+    if @question.update(question_params)
+      redirect_to question_answers_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    Question.find(params[:id]).delete
+    redirect_to authenticated_root_path
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :description, :upvote, :downvote, :question_id)
   end
 end
