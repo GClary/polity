34c957c30995e6fc6f8369d8e5125b250de3ef98
Grayson <grayson.clary@gmail.com>
2016-01-07 20:04:55 -0500

    Styles navbar links properly

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   authenticated :user do
     root 'home#index', as: :authenticated_root
   end
-  
+
   root to: redirect('users/sign_in')
 
   resources :questions do
