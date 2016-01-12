7f3145c6043542bbf45a79f0c2619c7fd70efc1d
Grayson <grayson.clary@gmail.com>
2016-01-12 18:15:30 -0500

    Trying to fix the mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'protected-forest-7218.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://protected-forest-7218.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
