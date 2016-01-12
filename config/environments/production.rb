8a98e9b19c21e1fd1327918db1bd157fac8a8004
Grayson <grayson.clary@gmail.com>
2016-01-12 18:58:55 -0500

    Still fiddling with mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'google.com',
+  domain:               'herokuapp.com',
   user_name:            ENV["GMAIL_USERNAME"],
   password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
