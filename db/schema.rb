be0a2cf2d7331339b2d8481e9d7c87cd951b4731
Grayson <grayson.clary@gmail.com>
2016-01-26 17:47:22 -0500

    Adds roles column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112040055) do
+ActiveRecord::Schema.define(version: 20160126223836) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "description"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20160112040055) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "role"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
